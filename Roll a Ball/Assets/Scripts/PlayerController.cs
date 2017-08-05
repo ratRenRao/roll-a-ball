dfe6c00ae72287999f93db675c040f9af1276612
Brandon Richardson <brandon.richardson@extendhealth.com>
2017-08-05 13:07:08 -0700

    Make initial commit. Work in progress

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // called before rendering a frame
+    void Update()
+    {
+        
+    }
+
+    // called before performing any physics calculations
+    private void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+        
+        rb.AddForce(movement * speed);     
+    }
+}
